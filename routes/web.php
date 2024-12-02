01f9350b1c441b1e33f8d3a6f286e8e9a5ff67f4
Riko Gunawan <rikogunawan100@gmail.com>
2024-12-02 17:26:23 +0700

    MVC + Database

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+use App\Http\Controllers\User;
+
+//Versi singkat
+Route::resource('users', User::class);
