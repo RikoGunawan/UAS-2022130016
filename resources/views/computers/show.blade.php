150bb6de3636635c6e210b1c00a275c928883e3c
Riko Gunawan <rikogunawan100@gmail.com>
2024-12-06 08:08:02 +0700

    UI Show

--- resources/views/computers/show.blade.php
+++ resources/views/computers/show.blade.php
@@ -1,38 +1,55 @@
+<style>
+    .float-right {
+        float: right;
+    }
+</style>
+
 @extends('layouts.app')
 
 @section('content')
     <div class="container">
         <main>
-            <h1>{{ $computer->name }}</h1>
+            <div class="d-flex justify-content-between">
+                <h1>{{ $computer->name }}</h1>
+                <img src="@if ($computer->tier == 'tier_1')
+                        {{ asset('images/pc_tier1.png') }}
+                    @elseif($computer->tier == 'tier_2')
+                        {{ asset('images/pc_tier2.jpg') }}
+                    @elseif($computer->tier == 'tier_3')
+                        {{ asset('images/pc_tier3.jpg') }}
+                    @else
+                        {{ Storage::url($computer->image_path) }}
+                    @endif"
+                    class="photo-frame-detail-img float-right">
+            </div>
             <p>
                 {{ $computer->specifications }}
             </p>
+            <div class="table-responsive">
+                <table class="table table-hover table-bordered">
+                    <tbody>
+                        <tr>
+                            <th scope="row" class="text-nowrap">ID</th>
+                            <td class="align-middle">{{ $computer->id }}</td>
 
-            <table class="table table-primary">
-                <tbody>
-                    <tr>
-                        <td><b>ID</b></td>
-                        <td>{{ $computer->id }}</td>
-                    </tr>
-                    <tr>
-                        <td><b>Location</b></td>
-                        <td>{{ $computer->location }}</td>
-                    </tr>
-                    <tr>
-                        <td><b>Tier</b></td>
-                        <td>{{ $computer->tier }}</td>
-                    </tr>
-                    <tr>
-                        <td><b>Status</b></td>
-                        <td>{{ $computer->status }}</td>
-                    </tr>
-                    <tr>
-                        <td><b>Image</b></td>
-                        <td><img src="{{ Storage::url($computer->image_path) }}" class="img-fluid" width="200" /></td>
-                    </tr>
-                </tbody>
-            </table>
+                        </tr>
+                        <tr>
+                            <th scope="row" class="text-nowrap">Location</th>
+                            <td class="align-middle">{{ $computer->location }}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row" class="text-nowrap">Tier</th>
+                            <td class="align-middle">{{ $computer->tier }}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row" class="text-nowrap">Status</th>
+                            <td class="align-middle">{{ $computer->status }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </main>
     </div>
 @endsection
+
 
